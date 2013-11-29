28c8897c9b86844d78740745899646e0399d27e4
ksaa189 <apple@appletekiMacBook-Pro.local>
2013-11-29 09:45:13 +0800

    Initial Commit

---
+++ XuanZhuan/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  XuanZhuan
+//
+//  Created by Leo on 13-11-29.
+//  Copyright (c) 2013年 DengBin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ XuanZhuan/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  XuanZhuan
+//
+//  Created by Leo on 13-11-29.
+//  Copyright (c) 2013年 DengBin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
